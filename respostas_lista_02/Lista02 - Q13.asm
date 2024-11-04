f8faadaf18bbdc271a3d7fd01aac108f91bc1dae
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-11-04 10:19:41 -0300

    Update Lista02 - Q13.asm

--- respostas_lista_02/Lista02 - Q13.asm
+++ respostas_lista_02/Lista02 - Q13.asm
@@ -2,26 +2,56 @@
 main:
 	addi $2 $0 5
 	syscall
-	addi $14 $0 10
-	div $2 $14
-	mfhi $16 #8
-	mflo $17
-	div $17 $14
-	mfhi $17 #0
-	mflo $18
-	div $18 $14
-	mfhi $18 #0
-	mflo $4 #2
+	add $8 $2 $0
+	add $9 $2 $0
+	
+	addi $10 $0 10
+	div $8 $10
+	mfhi $20 # 8
+	mflo $8
+	div $8 $10
+	mfhi $21 # 0
+	mflo $8
+	div $8 $10
+	mfhi $22 # 0
+	mflo $23 # 2
+
+	beq $23 $0 esp
+	add $4 $9 $0
 	addi $2 $0 1
 	syscall
-	add $4 $18 $0
+	j fim	
+esp:
+	addi $4 $0 ' '
+	addi $2 $0 11
+	syscall
+	
+	beq $22 $0 esp2
+	add $4 $9 $0
 	addi $2 $0 1
 	syscall
-	add $4 $17 $0
+	j fim
+	
+esp2:
+	addi $4 $0 ' '
+	addi $2 $0 11
+	syscall
+	
+	beq $21 $0 esp3
+	add $4 $9 $0
 	addi $2 $0 1
 	syscall
-	add $4 $16 $0
+	j fim
+	
+esp3:
+	addi $4 $0 ' '
+	addi $2 $0 11
+	syscall
+	
+	add $4 $9 $0
 	addi $2 $0 1
 	syscall
+
+fim:
 	addi $2 $0 10
 	syscall
