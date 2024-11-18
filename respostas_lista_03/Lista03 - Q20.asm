d1d095eb71015ee410e3f62cb1ad3542325b64f2
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-11-18 10:10:57 -0300

    Update Lista03 - Q20.asm

--- respostas_lista_03/Lista03 - Q20.asm
+++ respostas_lista_03/Lista03 - Q20.asm
@@ -1,82 +1,87 @@
 .text
 main:
-	add $23 $0 $0 # numero de picos
-	
-	addi $2 $0 5
-	syscall
-	add $16 $2 $0 # $16 => N = numero de amostras
-	
-	addi $2 $0 5
-	syscall
-	add $17 $2 $0 # primeiro valor
-	
-	addi $2 $0 5
-	syscall
-	add $18 $2 $0 # segundo valor
-	
-	add $4 $17 $0
-	add $5 $18 $0
-	
-	addi $19 $16 -2 # $19 <= N - 2
-test:
+      	addi $23 $0 0  # numero de picos
+
+      	addi $2 $0 5
+      	syscall
+      
+     	beq $2 $0 fim
+      
+      	add $16 $0 $2 # $16 => N = numero de amostras
+      
+      	addi $2 $0 5
+      	syscall
+      	add $17 $0 $2 # primeiro valor
+      
+      	addi $2 $0 5
+      	syscall
+      	add $18 $0 $2 # segundo valor
+     
+      	add $4 $0 $17    
+      	add $5 $0 $18
+     
+      	addi $19 $16 -2 #$19 <= N-2
+test: 
 	beq $19 $0 fimlaco
 	
-	addi $2 $0 5
-	syscall
-	add $6 $2 $0
-	
-	jal pico
-	
-	add $23 $23 $2
-	
-	add $4 $5 $0
-	add $5 $6 $0
-	
-	addi $19 $19 -1
-	j test
-
+      	addi $2 $0 5
+      	syscall
+      	add $6 $0 $2
+      	
+      	jal pico
+      	
+      	add $23 $23 $2
+      	
+      	add $4 $0 $5
+      	add $5 $0 $6
+      	
+      	addi $19 $19 -1
+      	j test
 fimlaco:
-	add $6 $17 $0
-	
-	jal pico
-	
-	
-	add $23 $23 $2
-	
-	add $4 $5 $0
-	add $5 $6 $0
-	add $6 $18 $0
-	
-	jal pico
-	
-	add $23 $23 $2
-	
-	add $4 $23 $0
-	addi $2 $0 1
-	syscall
-	
-fim:
-	addi $2 $0 10
-	syscall
-
-#======================================
-# funcao pico
+      	add $6 $0 $17
+      	
+      	jal pico
+      	
+      	add $23 $23 $2
+      	
+      	add $4 $0 $5
+      	add $5 $0 $6
+      	add $6 $0 $18
+      	
+      	jal pico
+      	
+      	add $23 $23 $2
+      	      
+      	add $4 $0 $23
+      	addi $2 $0 1
+      	syscall
+      	
+      	add $4 $0 '\n'
+      	addi $2 $0 11
+      	syscall
+      	
+      	j main
+ 
+terminou:
+      	addi $2 $0 10
+      	syscall
+#===================================      
+# Função pico
 # entrada: $4, $5, $6
 # saida: $2
-# reg usados: $8, $9, $10, $11
-# rotulos usados:
-
-pico:
-	slt $8 $4 $5 # $8 = $4 < $5 ? 1 : 0
-	slt $9 $6 $5 # $9 = $6 < $5 ? 1 : 0
-	
-	and $10 $8 $9
-	
-	slt $8 $5 $4 # $8 = $5 < $4 ? 1 : 0
-	slt $9 $5 $6 # $9 = $5 < $6 ? 1 : 0
-	
-	and $11 $8 $9
-	
-	or $2 $10 $11
-	
-	jr $31
+# Reg usados: $8, $9, $10, $11
+# Rotulos usados:      
+pico: 
+	slt $8, $4, $5 # $8 = $4<$5 ? 1 : 0
+      	slt $9, $6, $5 # $8 = $6<$5 ? 1 : 0
+      	
+      	and $10, $8, $9
+      	
+      	slt $8, $5, $4 # $8 = $5<$4 ? 1 : 0
+      	slt $9, $5, $6 # $8 = $5<$6 ? 1 : 0
+      	
+      	and $11, $8, $9
+      	
+      	or $2, $10, $11
+      	
+      	jr $31
